3c3b940fe5283558fb826305a91ef7ff01424af1
PedroTejon <pedrohtejon@gmail.com>
2023-11-24 05:24:17 -0300

    refactor: retirei coisas desnecessárias e botei o param que esqueci do get_followed_companies

--- modules/linkedin.py
+++ modules/linkedin.py
@@ -1,35 +1,31 @@
 from os import listdir
 from json import load, dump, loads
-from modules.utils import elemento_existe, query_selector, query_selector_all, filtrar_vaga, initialize_puppet
+from modules.utils import filtrar_vaga
 from time import sleep
 from requests import get, Session
 from bs4 import BeautifulSoup, PageElement
 from unidecode import unidecode
 from numpy import split, arange
 from re import sub
-from base64 import b64encode
-from urllib.parse import unquote
-from playwright.sync_api import Response, Route
 
 
 def scrape_vagas_recomendadas() -> [set, set]:
     cookies = load(open('data/cookies.json', 'r', encoding='utf-8'))
-    cookies_simplificados = {cookie['name']: cookie['value'].replace('\"', '') for cookie in cookies}
 
     sessao = Session()
     
     vagas = {}
-    vagas_recusadas = set()
+    vagas_recusadas = 0
     
     page = 0
     while True:
-        request = sessao.get(f'https://www.linkedin.com/jobs/collections/recommended/?start={25 * page}', cookies=cookies_simplificados)
+        request = sessao.get(f'https://www.linkedin.com/jobs/collections/recommended/?start={25 * page}', cookies=cookies)
         if request.status_code in [400, 429]:
             sleep(1)
             continue
         soup = BeautifulSoup(request.content, 'html.parser')
         
-        conta_atual = len(vagas) + len(vagas_recusadas)
+        conta_atual = len(vagas) + vagas_recusadas
         codigos = soup.find_all('code', id=lambda id: id and id.startswith("bpr-guid-"))
         for tag_codigo in codigos:
             resposta = loads(tag_codigo.get_text().replace('&quot;', '"').replace('&amp;', '&').replace('&#61;', '='))
@@ -46,20 +42,41 @@ def scrape_vagas_recomendadas() -> [set, set]:
                         
                         vagas[codigo] = {'titulo': titulo, 'local': local, 'empresa': empresa, 'modalidade': modalidade}
                     else:
-                        vagas_recusadas.add(titulo)
+                        vagas_recusadas += 1
         
-        if conta_atual >= len(vagas) + len(vagas_recusadas):
+        if conta_atual >= len(vagas) + vagas_recusadas:
             break
 
         sleep(1)
         page += 1
 
-    return [vagas, vagas_recusadas]
+    return vagas
+
+
+def extrair_vagas(itens: list) -> dict:
+    vagas = {}
+
+    for vaga in itens:
+        codigo_entidade: PageElement = vaga.find('div', {'class': 'base-card'})
+        if not codigo_entidade:
+            codigo_entidade = vaga.find('a', {'class': 'base-card'})
+        
+        codigo: str = codigo_entidade['data-entity-urn'].replace('urn:li:jobPosting:', '')
+        if codigo not in vagas:
+            titulo = sub(r' {2,}|\n', '', vaga.find('h3', {'class': 'base-search-card__title'}).get_text().strip())
+            local = vaga.find('span', {'class': 'job-search-card__location'}).get_text().strip()
+
+            modalidade: str = 'Remoto' if 'Remoto' in local else 'Presencial/Hibrido'
+            if filtrar_vaga(sub(r'[\[\]\(\),./\\| ]+', ' ', unidecode(titulo).lower()), local, modalidade):
+                empresa = vaga.find('a', {'class': 'hidden-nested-link'}).get_text().strip()
+                
+                vagas[codigo] = {'titulo': titulo, 'local': local, 'empresa': empresa, 'modalidade': modalidade}
+    
+    return vagas
 
 
 def scrape_vagas_empresas(lista_empresas: dict) -> [dict, set]:
     vagas = {}
-    vagas_recusadas = set()
 
     for lista in split(list(lista_empresas), arange(10, len(list(lista_empresas)), 10)):
         empresas_code = '%2C'.join([lista_empresas[empresa]['id'] for empresa in lista])
@@ -79,29 +96,16 @@ def scrape_vagas_empresas(lista_empresas: dict) -> [dict, set]:
             if not itens:
                 break
 
-            for vaga in itens:
-                codigo = vaga.find('div', {'class': 'base-card'})['data-entity-urn'].replace('urn:li:jobPosting:', '')
-                if codigo not in vagas:
-                    titulo = sub(r' {2,}|\n', '', vaga.find('h3', {'class': 'base-search-card__title'}).get_text().strip())
-                    local = vaga.find('span', {'class': 'job-search-card__location'}).get_text().strip()
+            vagas |= extrair_vagas(itens)
 
-                    modalidade = 'Remoto' if 'Remoto' in local else 'Presencial/Hibrido'
-                    if filtrar_vaga(sub(r'[\[\]\(\),./\\| ]+', ' ', unidecode(titulo).lower()), local,  modalidade):
-                        empresa = vaga.find('a', {'class': 'hidden-nested-link'}).get_text().strip()
-                        
-                        vagas[codigo] = {'titulo': titulo, 'local': local, 'empresa': empresa, 'modalidade': modalidade}
-                    else:
-                        vagas_recusadas.add(titulo)
-                
             sleep(1)
             page += 1
 
-    return [vagas, vagas_recusadas]
+    return vagas
 
 
 def scrape_vagas_remotos() -> [dict, set]:
     vagas = {}
-    vagas_recusadas = set()
 
     page = 0
     while True:
@@ -119,37 +123,22 @@ def scrape_vagas_remotos() -> [dict, set]:
         itens = soup.find_all('li')
         if not itens:
             break
-        for vaga in itens:
-            codigo_entidade: PageElement = vaga.find('div', {'class': 'base-card'})
-            if not codigo_entidade:
-                codigo_entidade = vaga.find('a', {'class': 'base-card'})
-            
-            codigo: str = codigo_entidade['data-entity-urn'].replace('urn:li:jobPosting:', '')
-            if codigo not in vagas:
-                titulo = sub(r' {2,}|\n', '', vaga.find('h3', {'class': 'base-search-card__title'}).get_text().strip())
-                local = vaga.find('span', {'class': 'job-search-card__location'}).get_text().strip()
 
-                if filtrar_vaga(sub(r'[\[\]\(\),./\\| ]+', ' ', unidecode(titulo).lower()), local, 'Remoto'):
-                    empresa = vaga.find('a', {'class': 'hidden-nested-link'}).get_text().strip()
-                    
-                    vagas[codigo] = {'titulo': titulo, 'local': local, 'empresa': empresa, 'modalidade': 'Remoto'}
-                else:
-                    vagas_recusadas.add(titulo)
+        vagas |= extrair_vagas(itens)
             
         sleep(1)
         page += 1
 
-    return [vagas, vagas_recusadas]
+    return vagas
 
 
 # def scrape_detalhes_vagas(vagas: dict) -> [dict]:        
 #     cookies = load(open('data/cookies.json', 'r', encoding='utf-8'))
-#     cookies_simplificados = {cookie['name']: cookie['value'].replace('\"', '') for cookie in cookies}
 
 #     sessao = Session()
     
 #     for vaga in vagas:
-#         detalhes_vaga = sessao.get(f'https://www.linkedin.com/jobs/view/{vaga}/', cookies=cookies_simplificados)
+#         detalhes_vaga = sessao.get(f'https://www.linkedin.com/jobs/view/{vaga}/', cookies=cookies)
 #         if detalhes_vaga.status_code in [400, 429]:
 #             sleep(1)
 #             continue
@@ -160,20 +149,15 @@ def scrape_vagas_remotos() -> [dict, set]:
 #             .split('&')[0])    
 
 
-def save_company_images(response: Response):
-    global imagens
-    if response.header_value('content-type') == 'image/jpeg' and 'company-logo' in response.url:
-        imagens[response.url] = b64encode(response.body()).decode('ascii')
-
-
 def get_followed_companies() -> dict:
     empresas = {}
     cookies = load(open('data/cookies.json', 'r', encoding='utf-8'))
-    cookies_simplificados = {cookie['name']: cookie['value'].replace('\"', '') for cookie in cookies}
+    armazenamento_local = load(open('data/armazenamento_local.json', 'r', encoding='utf-8'))
 
     sessao = Session()
     
-    detalhes_empresas_iniciais = sessao.get(f'https://www.linkedin.com/voyager/api/graphql?variables=(start:0,count:100,paginationToken:null,pagedListComponent:urn%3Ali%3Afsd_profilePagedListComponent%3A%28ACoAADNkf4EBpG80N1CmufGBMrJ8O3nrwFhhedY%2CINTERESTS_VIEW_DETAILS%2Curn%3Ali%3Afsd_profileTabSection%3ACOMPANIES_INTERESTS%2CNONE%2Cpt_BR%29)&queryId=voyagerIdentityDashProfileComponents.3efef764c5f936e8a825b8674c814b0c', cookies=cookies_simplificados, headers={'csrf-token': cookies_simplificados['JSESSIONID']})
+    profile_id = loads(armazenamento_local["voyager-web:badges"])[0]["_id"]
+    detalhes_empresas_iniciais = sessao.get(f'https://www.linkedin.com/voyager/api/graphql?variables=(start:0,count:100,paginationToken:null,pagedListComponent:urn%3Ali%3Afsd_profilePagedListComponent%3A%28{profile_id}%2CINTERESTS_VIEW_DETAILS%2Curn%3Ali%3Afsd_profileTabSection%3ACOMPANIES_INTERESTS%2CNONE%2Cpt_BR%29)&queryId=voyagerIdentityDashProfileComponents.3efef764c5f936e8a825b8674c814b0c', cookies=cookies, headers={'csrf-token': cookies['JSESSIONID']})
     data = loads(detalhes_empresas_iniciais.content.decode('utf-8'))
     for elemento in data['data']['identityDashProfileComponentsByPagedListComponent']['elements']:
         entity = elemento['components']['entityComponent']
@@ -196,7 +180,7 @@ def get_followed_companies() -> dict:
     total = data['data']['identityDashProfileComponentsByPagedListComponent']['paging']['total']
 
     for i in range(100, total, 100):
-        detalhes_empresas_restantes = sessao.get(f'https://www.linkedin.com/voyager/api/graphql?variables=(start:{i},count:100,paginationToken:null,pagedListComponent:urn%3Ali%3Afsd_profilePagedListComponent%3A%28ACoAADNkf4EBpG80N1CmufGBMrJ8O3nrwFhhedY%2CINTERESTS_VIEW_DETAILS%2Curn%3Ali%3Afsd_profileTabSection%3ACOMPANIES_INTERESTS%2CNONE%2Cpt_BR%29)&queryId=voyagerIdentityDashProfileComponents.3efef764c5f936e8a825b8674c814b0c', cookies=cookies_simplificados, headers={'csrf-token': cookies_simplificados['JSESSIONID']})
+        detalhes_empresas_restantes = sessao.get(f'https://www.linkedin.com/voyager/api/graphql?variables=(start:{i},count:100,paginationToken:null,pagedListComponent:urn%3Ali%3Afsd_profilePagedListComponent%3A%28ACoAADNkf4EBpG80N1CmufGBMrJ8O3nrwFhhedY%2CINTERESTS_VIEW_DETAILS%2Curn%3Ali%3Afsd_profileTabSection%3ACOMPANIES_INTERESTS%2CNONE%2Cpt_BR%29)&queryId=voyagerIdentityDashProfileComponents.3efef764c5f936e8a825b8674c814b0c', cookies=cookies, headers={'csrf-token': cookies['JSESSIONID']})
         data = loads(detalhes_empresas_restantes.content.decode('utf-8'))
 
         for elemento in data['data']['identityDashProfileComponentsByPagedListComponent']['elements']:
@@ -217,6 +201,7 @@ def get_followed_companies() -> dict:
             }
 
     dump(empresas, open('data/linkedin_followed.json', 'w', encoding='utf-8'), ensure_ascii=False)
+    return empresas
 
 
 def get_jobs(env: dict, update_followed: bool = False):
@@ -226,25 +211,17 @@ def get_jobs(env: dict, update_followed: bool = False):
         lista_empresas = load(open('data/linkedin_followed.json', 'r', encoding='utf-8'))
 
     vagas = {}
-    vagas_recusadas = set()
 
     res_vagas_empresas = scrape_vagas_empresas(lista_empresas)
-    vagas |= res_vagas_empresas[0]
-    vagas_recusadas.update(res_vagas_empresas[1])
+    vagas |= res_vagas_empresas
 
     res_vagas_recomendadas = scrape_vagas_recomendadas()
-    vagas |= res_vagas_recomendadas[0]
-    vagas_recusadas.update(res_vagas_recomendadas[1])
+    vagas |= res_vagas_recomendadas
 
     res_vagas_recentes_remo = scrape_vagas_remotos()
-    vagas |= res_vagas_recentes_remo[0]
-    vagas_recusadas.update(res_vagas_recentes_remo[1])
-
-
+    vagas |= res_vagas_recentes_remo
 
     dump(vagas, open('data/vagas_accepted.json', 'w', encoding='utf-8'), ensure_ascii=False)
-    with open('data/vagas_refused.txt', 'w', encoding='utf-8') as file:
-        file.write('\n'.join(vagas_recusadas))
 
     # pegar info das vagas por requests
     #f'https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/3705238738'
