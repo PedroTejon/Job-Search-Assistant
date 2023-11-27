dda3131bba0d521afb491c525f5048579f1df876
PedroTejon <pedrohtejon@gmail.com>
2023-11-27 11:24:50 -0300

    feat: auth_setup espera resposta 200 de login

--- auth_setup.py
+++ auth_setup.py
@@ -3,15 +3,25 @@ from json import dump, dumps
 
 def setup():
     with initialize_puppet() as driver:
-        driver.goto('https://www.linkedin.com/')
-        input('Logue no LinkedIn para salvarmos sua sessão e aperte Enter')
-        
-        cookies = driver.context.cookies()
-        dump({cookie['name']: cookie['value'].replace('\"', '') for cookie in cookies}, open('data/cookies.json', 'w', encoding='utf-8'), ensure_ascii=False)
+        print('Logue com sua conta do LinkedIn')      
+        with driver.expect_response(lambda x: 'https://www.linkedin.com/feed/?trk=homepage-basic_sign-in-submit' in x.url and x.status == 200, timeout=0):
+            driver.goto('https://www.linkedin.com/')
+        print('Conta do LinkedIn conectada com sucesso')
 
+        print('Logue com sua conta do Glassdoor')
+        with driver.expect_response(lambda x: 'https://www.glassdoor.com.br/Vaga/index.htm' in x.url and x.status == 200, timeout=0):
+            driver.goto('https://www.glassdoor.com.br/')
+        print('Conta do Glassdoor conectada com sucesso')
+
+        print('Salvando Cookies...')
+        cookies = driver.context.cookies()
+        dump(cookies, open('data/cookies.json', 'w', encoding='utf-8'), ensure_ascii=False)
+
+        print('Salvando Local Storage...')
         armazenamento_local = driver.evaluate("() => {var ls = window.localStorage, items = {}; for (var i = 0, k; i < ls.length; ++i)  items[k = ls.key(i)] = ls.getItem(k); return items;}")
         dump(armazenamento_local, open('data/armazenamento_local.json', 'w', encoding='utf-8'), ensure_ascii=False)
 
+        print('Autenticação concluída')
 
 if __name__ == '__main__':
     setup()
