c4397cd63d32bbb1c1bacb819d364025948ca203
PedroTejon <pedrohtejon@gmail.com>
2023-12-05 15:25:23 -0300

    fix: não é necessário inicializar o servidor django para autenticação

--- auth_setup.py
+++ auth_setup.py
@@ -1,6 +1,23 @@
+from playwright.sync_api import sync_playwright, Page
 from bs4 import BeautifulSoup
-from modules.utils import initialize_puppet
-from json import dump, dumps, loads
+from json import dump, load, loads
+
+
+def initialize_puppet(load_cookies=False, headless=False) -> Page:
+    playwright =  sync_playwright().start()
+    browser = playwright.chromium.launch(args=['--no-sandbox', '--disable-dev-shm-usage', '--incognito', '--disable-blink-features=AutomationControlled', '--disable-gpu', '--disable-extensions', '--ignore-certificate-errors-spki-list', '--no-default-browser-check', '--window-size=1280,720'],
+                                ignore_default_args=['--enable-automation'], 
+                                headless=headless)
+    context = browser.new_context(viewport=None, user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79')
+    context.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    driver: Page = context.new_page()
+
+    if load_cookies:
+        cookies = load(open('data/cookies.json', 'r', encoding='utf-8'))
+        driver.context.add_cookies(cookies)
+    
+    return driver
+
 
 def setup():
     with initialize_puppet() as driver:
