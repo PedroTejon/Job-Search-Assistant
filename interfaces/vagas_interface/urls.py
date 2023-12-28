3366ebecafc1fa926c73201f94ae0465365328d4
PedroTejon <pedrohtejon@gmail.com>
2023-12-28 20:33:03 -0300

    feat(interface de vagas): add funções de search por termo e paginação por meio de parâmetros da URL

--- interfaces/vagas_interface/urls.py
+++ interfaces/vagas_interface/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("page/<int:page>/", views.pagination, name="pagination"),
-    path("teste2/", views.teste2, name="teste2"),
+    path("", views.index, name="index")
 ]
--- interfaces/vagas_interface/views.py
+++ interfaces/vagas_interface/views.py
@@ -1,46 +1,39 @@
-import threading
-
-from numpy import split, arange
-from django.http import HttpResponse, JsonResponse
+from django.db.models.query import Q
+from django.http import HttpResponse
 from django.template import loader
+from numpy import arange, split
 
-
-from interfaces.vagas_interface.models import Listing, Company
+from interfaces.vagas_interface.models import Company, Listing
 
 
 def index(request):
     template = loader.get_template('index.html')
-    listings = Listing.objects.values()[:min(Listing.objects.count(), 50)] if Listing.objects.count() >= 1 else []
-    return HttpResponse(template.render({'listings': listings}, request))
+    page = int(request.GET.get('page', 1))
+    queries_str = request.GET.get('query', '')
+    queries = queries_str.split()
+    try:
+        if not queries:
+            pages = split(Listing.objects.values(), arange(50, Listing.objects.count(), 50))
+            listings = list(list(pages)[page - 1])
+        else:
+            query = Q()
+            for term in queries:
+                query &= Q(title__contains=term)
+            pages = split(Listing.objects.filter(query).values(), arange(50, Listing.objects.count(), 50))
+            listings = list(list(pages)[page - 1])
 
+        for listing in listings:
+            company = Company.objects.get(id__exact=listing['company_id'])
+            if listing['platform'] == 'LinkedIn':
+                listing['company_name'] = company.platforms['linkedin']['name']
+            elif listing['platform'] == 'Glassdoor':
+                listing['company_name'] = company.platforms['glassdoor']['name']
+            elif listing['platform'] == 'Catho':
+                listing['company_name'] = company.platforms['catho']['name']
+            elif listing['platform'] == 'Vagas.com':
+                listing['company_name'] = company.platforms['vagas_com']['name']
 
-def pagination(request, page):
-    template = loader.get_template('index.html')
-    if page >= 1:
-        try:
-            listings = list(split(Listing.objects.values(), arange(50, Listing.objects.count(), 50)))[page - 1]
-            for listing in listings:
-                company = Company.objects.get(id__exact=listing['company_id'])
-                if listing['platform'] == 'LinkedIn':
-                    listing['company_name'] = company.platforms['linkedin']['name']
-                elif listing['platform'] == 'Glassdoor':
-                    listing['company_name'] = company.platforms['glassdoor']['name']
-                elif listing['platform'] == 'Catho':
-                    listing['company_name'] = company.platforms['catho']['name']
-                elif listing['platform'] == 'Vagas.com':
-                    listing['company_name'] = company.platforms['vagas_com']['name']
-            return HttpResponse(template.render({'listings': listings}, request))
-        except IndexError:
-            return HttpResponse(template.render({'listings': []}, request))
-
-    return HttpResponse(template.render({'listings': []}, request))
-
-
-def teste1(request):
-    if not any(filter(lambda x: x.name == 'scraper', threading.enumerate())):
-        pass
-    return JsonResponse({'teste': 'Hello, trestee1index.'})
-
-
-def teste2(request):
-    return JsonResponse({'teste': 'teste2222.'})
+        paginations = list(range(max(1, page - 4), min(page + 4, len(pages) + 1)))
+        return HttpResponse(template.render({'listings': listings, 'page': page, 'pages': paginations, 'total_pages': len(pages) - 1, 'query': queries_str}, request))
+    except IndexError:
+        return HttpResponse(template.render({'listings': [], 'page': page, 'pages': [page], 'total_pages': 0, 'query': queries_str}, request))
