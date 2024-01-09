51e96e4bf684d499b0530302aff53d4ca750a96e
PedroTejon <pedrohtejon@gmail.com>
2024-01-08 22:34:04 -0300

    feat: mudança leve de aparência do header e criação de pasta global de arquivos estáticos

--- django_config/settings.py
+++ django_config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / 'interfaces/static']
 DIRS = (
     BASE_DIR / 'interfaces/templates/',
 )
