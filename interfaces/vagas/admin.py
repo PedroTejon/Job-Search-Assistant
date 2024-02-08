be9d4d48d78ad4649b21e7b5299c239e3a2325f4
PedroTejon <pedrohtejon@gmail.com>
2024-02-08 12:36:33 -0300

    chore: settei os campos de pesquisa na interface admin

--- interfaces/vagas/admin.py
+++ interfaces/vagas/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Company, Listing
 
-admin.site.register(Listing)
-admin.site.register(Company)
+# user: admin, pass: admin
+
+
+class ListingAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'platform_id')
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ('id',)
+
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Company, CompanyAdmin)
